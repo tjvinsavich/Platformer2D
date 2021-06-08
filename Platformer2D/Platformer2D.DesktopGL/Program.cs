53087d33998f03eb4f73c6f925b20a61a7820d7a
TJ Vinsavich <tjvinsavich@gmail.com>
2021-06-08 14:35:24 -0500

    initial commit

---
+++ Platformer2D/Platformer2D.DesktopGL/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Platformer2D.DesktopGL
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            // TODO: Create a new instance of the PlatformerGame Class called game (Tip: instantiating an object of type PlatformerGame)
+            var game = new PlatformerGame();
+
+            // TODO: Call the Run() method of the game object to run the game (Tip: accessing an object's members using dot notation)
+            game.Run();
+        }
+    }
+}
